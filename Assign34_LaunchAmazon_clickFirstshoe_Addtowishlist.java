b45ba180eeb4af08a80f5f375bc10884ab960ae2
RiteshSingh1997 <riteshsingh0689@gmail.com>
2024-11-05 12:43:36 +0530

    Update Assign34_LaunchAmazon_clickFirstshoe_Addtowishlist.java

--- Assign34_LaunchAmazon_clickFirstshoe_Addtowishlist.java
+++ Assign34_LaunchAmazon_clickFirstshoe_Addtowishlist.java
@@ -21,16 +21,16 @@ public class Assign34_LaunchAmazon_clickFirstshoe_Addtowishlist
 		e1.sendKeys(Keys.ENTER);
 		WebElement e2 = driver.findElement(By.xpath("(//a[@class='a-link-normal s-no-outline'])[1]"));
 		e2.click();
-//		Set<String> s1 = driver.getWindowHandles();
-//		System.out.println(s1);
-//		Iterator<String> i1 = s1.iterator();
-//		String parentid = i1.next();
-//		String childid = i1.next();
-//		System.out.println(parentid);
-//		System.out.println(childid);
-//		driver.switchTo().window(childid);
-//		WebElement e3 = driver.findElement(By.id("wishListMainButton"));
-//		e3.click();	
+		Set<String> s1 = driver.getWindowHandles();
+		System.out.println(s1);
+		Iterator<String> i1 = s1.iterator();
+		String parentid = i1.next();
+		String childid = i1.next();
+		System.out.println(parentid);
+		System.out.println(childid);
+		driver.switchTo().window(childid);
+		WebElement e3 = driver.findElement(By.id("wishListMainButton"));
+		e3.click();	
 
 	}
 
